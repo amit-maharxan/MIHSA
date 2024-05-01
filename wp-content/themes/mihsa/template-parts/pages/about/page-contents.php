287e71e9bb8755e435d6553ca75934f841205151
Suyogya Luitel <lsuyogya@gmail.com>
2024-05-01 17:34:59 +0545

    Design: Policy Desktop

--- wp-content/themes/mihsa/template-parts/pages/about/page-contents.php
+++ wp-content/themes/mihsa/template-parts/pages/about/page-contents.php
@@ -2,9 +2,8 @@
       <div class="fullWidthOuter banner">
             <?php $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID), 'thumbnail' ); ?>
             <img src="<?php echo $url;?>" alt="" class="fullCol"/>
-
             <div class="absoluteCenter color-light">
-                  <h1 class="heading-title-sm margin-reset upper">About</h1>
+                  <h1 class="heading-title-sm margin-reset upper"><?php the_title();?></h1>
                   <div class="breadcrumb">
                         <ul class="flexList">
                               <li><a href="<?php echo site_url();?>">Home</a></li>
