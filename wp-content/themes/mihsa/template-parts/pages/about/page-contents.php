cb9c48afe1a152f4d2ddf6323e5bd30beb4ff3de
Suyogya Luitel <lsuyogya@gmail.com>
2024-05-01 16:01:12 +0545

    Design: About (no polka pattern)

--- wp-content/themes/mihsa/template-parts/pages/about/page-contents.php
+++ wp-content/themes/mihsa/template-parts/pages/about/page-contents.php
@@ -1,32 +1,39 @@
 <section class="mainGrid">
-      <div class="halfWidthOuter inner-page-banner">
+      <div class="fullWidthOuter banner">
             <?php $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID), 'thumbnail' ); ?>
             <img src="<?php echo $url;?>" alt="" class="fullCol"/>
 
-            <div class="breadcrumb">
-                  <ul>
-                        <li><a href="<?php echo site_url();?>">Home</a></li>
-                        <li>/</li>
-                        <li><?php the_title();?></li>
-                  </ul>
+            <div class="absoluteCenter color-light">
+                  <h1 class="heading-title-sm margin-reset upper">About</h1>
+                  <div class="breadcrumb">
+                        <ul class="flexList">
+                              <li><a href="<?php echo site_url();?>">Home</a></li>
+                              <li>/</li>
+                              <li><?php the_title();?></li>
+                        </ul>
+                  </div>
+                  <!-- <div class="squareThingy"></div> -->
             </div>
       </div>
 </section>
 
 <section class="mainGrid">
-      <div class="section about-contents">
-            <div class="image">
-                  <img src="<?php the_field('about_image');?>" alt="" />
-            </div>
-            <div class="contents">
-                  <p class="about-contents">
-                        <?php the_content();?>
-                  </p>
-                  <div class="certification-images">
-                        <?php if( have_rows('certification_images') ):
+      <div class="content">
+            <div class="fullCol about-contents">
+                  <div class="imgWrapper">
+                        <img src="<?php the_field('about_image');?>" alt="" />
+                  </div>
+                  <div class="contents">
+                        <!-- Content is wrapped in paragraphs, styled accordingly -->
+                        <!-- <p class="about-contents"> -->
+                              <?php the_content();?>
+                        <!-- </p> -->
+                        <div class="certification-images">
+                              <?php if( have_rows('certification_images') ):
                         while( have_rows('certification_images') ) : the_row(); ?>
                               <img src="<?php the_sub_field('image');?>" alt="" />
-                        <?php endwhile; endif; ?>
+                              <?php endwhile; endif; ?>
+                        </div>
                   </div>
             </div>
       </div>
