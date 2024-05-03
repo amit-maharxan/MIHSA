acdc429f7eb132a726975c8cf280a16b2bbc3e34
Suyogya Luitel <lsuyogya@gmail.com>
2024-05-03 18:10:23 +0545

    testimonial grid

--- wp-content/themes/mihsa/archive-testimonials.php
+++ wp-content/themes/mihsa/archive-testimonials.php
@@ -1,23 +1,27 @@
 <?php do_action('mihsa_header'); ?>
 
+
 <section class="mainGrid">
-      <div class="halfWidthOuter inner-page-banner">
-            <?php $url = wp_get_attachment_url( get_post_thumbnail_id(150), 'thumbnail' ); ?>
+      <div class="fullWidthOuter banner-sm">
+            <?php $url = wp_get_attachment_url( get_post_thumbnail_id(156), 'thumbnail' ); ?>
             <img src="<?php echo $url;?>" alt="" class="fullCol"/>
-            <h1 class="post-type-title">TESTIMONIALS</h1>
-            <div class="breadcrumb">
-                  <ul>
-                        <li><a href="<?php echo site_url();?>">Home</a></li>
-                        <li>/</li>
-                        <li>Testimonials</li>
-                  </ul>
+            <div class="absoluteCenter color-light">
+                  <h1 class="heading-title-sm margin-reset upper">Home</h1>
+                  <div class="breadcrumb">
+                        <ul class="flexList">
+                              <li><a href="<?php echo site_url();?>">Testimonials</a></li>
+                              <li>/</li>
+                              <li><?php the_title();?></li>
+                        </ul>
+                  </div>
+                  <!-- <div class="squareThingy"></div> -->
             </div>
       </div>
 </section>
 
 <section class="mainGrid">
   <div class="content">
-    <div class="testimonials-lists">
+    <div class="testimonials-lists testimonialSectionGrid">
       <?php
       $wp_query = new WP_Query(array(
           'post_type'       => 'testimonials', // Fetch regular WordPress posts
@@ -27,7 +31,8 @@
       // Loop through the WordPress posts
       while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
         <div class="testimonialWrapper">
-            <img src="<?php echo wp_get_attachment_url( get_post_thumbnail_id($post->ID) );?>" alt="" />
+            <!-- <img src="<?php echo wp_get_attachment_url( get_post_thumbnail_id($post->ID) );?>" alt="" /> -->
+            <img src="http://localhost/mihsa/wp-content/uploads/2024/04/FacialFillersImg.png" alt="" />
             <div class="right-testimonial-contents">
               <p><?php the_content();?></p>
               <span class="bold"><?php the_title();?></span>
