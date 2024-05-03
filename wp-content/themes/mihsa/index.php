25c8b18cc38c86cca4c44dd737380d44435f699c
Suyogya Luitel <lsuyogya@gmail.com>
2024-05-03 17:02:46 +0545

    blog

--- wp-content/themes/mihsa/index.php
+++ wp-content/themes/mihsa/index.php
@@ -1,23 +1,26 @@
 <?php do_action('mihsa_header'); ?>
 
 <section class="mainGrid">
-      <div class="halfWidthOuter inner-page-banner">
+      <div class="fullWidthOuter banner-sm">
             <?php $url = wp_get_attachment_url( get_post_thumbnail_id(156), 'thumbnail' ); ?>
             <img src="<?php echo $url;?>" alt="" class="fullCol"/>
-            <h1 class="post-type-title">BLOG</h1>
-            <div class="breadcrumb">
-              <ul>
-                <li><a href="<?php echo site_url();?>">Home</a></li>
-                <li>/</li>
-                <li>Blog</li>
-              </ul>
+            <div class="absoluteCenter color-light">
+                  <h1 class="heading-title-sm margin-reset upper">BLOG</h1>
+                  <div class="breadcrumb">
+                        <ul class="flexList">
+                              <li><a href="<?php echo site_url();?>">Home</a></li>
+                              <li>/</li>
+                              <li><?php the_title();?></li>
+                        </ul>
+                  </div>
+                  <!-- <div class="squareThingy"></div> -->
             </div>
       </div>
 </section>
 
 <section class="mainGrid">
   <div class="content">
-    <div class="testimonials-lists">
+    <div class="testimonials-lists blogGrid">
       <?php
       $wp_query = new WP_Query(array(
           'post_type'       => 'post', // Fetch regular WordPress posts
@@ -29,10 +32,12 @@
         <div class="testimonialWrapper">
           <a href="<?php the_permalink();?>">
             <img src="<?php echo wp_get_attachment_url( get_post_thumbnail_id($post->ID) );?>" alt="" />
-            <div class="right-testimonial-contents">
-              <span class="bold"><?php the_title();?></span>
-            </div>
-          </a>
+            <!-- <img src="http://localhost/mihsa/wp-content/uploads/2024/04/FacialFillersImg.png" alt="" /> -->
+            <div class="blog-details">
+              <p class="added-on"><?php echo get_the_date();?></p>
+            <h2 class="title"><?php the_title();?></h2>
+          </div>
+        </a>
         </div>
       <?php endwhile; wp_reset_query();?>
     </div>
