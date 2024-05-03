f2af574af85a0cf48a713da9bd207cda31b258f4
Suyogya Luitel <lsuyogya@gmail.com>
2024-05-03 18:11:42 +0545

    blog layout snap

--- wp-content/themes/mihsa/single.php
+++ wp-content/themes/mihsa/single.php
@@ -1,7 +1,7 @@
 <?php do_action('mihsa_header'); ?>
 
-<section class="blog-section">
-      <div class="container">
+<section class="blog-section mainGrid">
+      <div class="container content">
         <div class="row align-items-center justify-content-between">
           <div id="blog-image" class="">
             <?php $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID), 'thumbnail' ); ?>
