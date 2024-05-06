23988e47d10769e1709eb1e9e50f1046632a96db
Suyogya Luitel <lsuyogya@gmail.com>
2024-05-06 12:51:54 +0545

    blog

--- wp-content/themes/mihsa/single.php
+++ wp-content/themes/mihsa/single.php
@@ -3,13 +3,15 @@
 <section class="blog-section mainGrid">
       <div class="container content">
         <div class="row align-items-center justify-content-between">
-          <div id="blog-image" class="">
-            <?php $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID), 'thumbnail' ); ?>
+          <div id="blog-image" class="blog-image">
+            <!-- <?php $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID), 'thumbnail' ); ?> -->
+            <?php $url = "http://localhost/mihsa/wp-content/uploads/2024/04/FacialFillersImg.png" ; ?>
             <img src="<?php echo $url;?>" alt="image">
           </div>
           <div class="blog-details">
-            <h2><?php the_title();?></h2>
+            <h2 class="titleFancy"><?php the_title();?></h2>
             <p class="added-on">Added on: <?php echo get_the_date();?></p>
+            <br><br>
             <p class="desc"><?php the_content();?></p>
           </div>
         </div>
