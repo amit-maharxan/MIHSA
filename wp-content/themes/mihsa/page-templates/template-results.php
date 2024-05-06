377585a1ad20660e882b55885b57697727bcd945
Suyogya Luitel <lsuyogya@gmail.com>
2024-05-06 12:25:03 +0545

    url fix-results

--- wp-content/themes/mihsa/page-templates/template-results.php
+++ wp-content/themes/mihsa/page-templates/template-results.php
@@ -8,8 +8,8 @@
 
 <section class="mainGrid">
       <div class="fullWidthOuter banner-sm">
-            <!-- <?php $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID), 'thumbnail' ); ?> -->
-            <?php $url = "https://mihsaaesthetics.com/dev/wp-content/uploads/2024/05/Rectangle-99-3.png" ; ?>
+        <!-- <?php $url = "https://mihsaaesthetics.com/dev/wp-content/uploads/2024/05/Rectangle-99-3.png" ; ?> -->
+            <?php $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID), 'thumbnail' ); ?>
             <img src="<?php echo $url;?>" alt="" class="fullCol"/>
             <div class="absoluteCenter color-light">
                   <h1 class="heading-title-sm margin-reset upper"><?php the_title();?></h1>
@@ -43,13 +43,13 @@
           <a class="no-hover" href="<?php the_permalink();?>">
             <div class="imgWrappers">
               <div class="imgWrapper">
-                <!-- <img class="before_image" src="<?php the_field('before_image');?>" alt="" /> -->
-                <img class="before_image" src="https://mihsaaesthetics.com/dev/wp-content/uploads/2024/05/Rectangle-869.png" alt="" />
+                <img class="before_image" src="<?php the_field('before_image');?>" alt="" />
+                <!-- <img class="before_image" src="https://mihsaaesthetics.com/dev/wp-content/uploads/2024/05/Rectangle-869.png" alt="" /> -->
                 </div>
                 <div class="imgWrapper">
 
-                  <!-- <img class="after_image" src="<?php the_field('after_image');?>" alt="" /> -->
-                  <img class="after_image" src="https://mihsaaesthetics.com/dev/wp-content/uploads/2024/05/Rectangle-868.png" alt="" />
+                  <img class="after_image" src="<?php the_field('after_image');?>" alt="" />
+                  <!-- <img class="after_image" src="https://mihsaaesthetics.com/dev/wp-content/uploads/2024/05/Rectangle-868.png" alt="" /> -->
                 </div>
               <span class="titleFancy"><?php the_title();?></span>
             </div>
